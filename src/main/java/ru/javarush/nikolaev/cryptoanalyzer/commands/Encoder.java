f493f923ec677189818471da66f294f057bd5a5b
a.nikolaev <sparkton02@gmail.com>
2022-03-21 22:36:10 +0500

    Forget add working classes

---
+++ src/main/java/ru/javarush/nikolaev/cryptoanalyzer/commands/Encoder.java
@@ -0,0 +1,11 @@
+package ru.javarush.nikolaev.cryptoanalyzer.commands;
+
+import ru.javarush.nikolaev.cryptoanalyzer.entity.Result;
+
+public class Encoder implements Action
+{
+    @Override
+    public Result run(String[] parameters) {
+        return null;
+    }
+}
