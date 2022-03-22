97ed93d66faf8bf060f1e9fee974db787ff7ccb2
a.nikolaev <sparkton02@gmail.com>
2022-03-22 21:43:38 +0500

    Change algorithm Decode and Encode

--- src/main/java/ru/javarush/nikolaev/cryptoanalyzer/commands/Encoder.java
+++ src/main/java/ru/javarush/nikolaev/cryptoanalyzer/commands/Encoder.java
@@ -4,92 +4,49 @@ import ru.javarush.nikolaev.cryptoanalyzer.constants.Constants;
 import ru.javarush.nikolaev.cryptoanalyzer.entity.Result;
 import ru.javarush.nikolaev.cryptoanalyzer.entity.ResultCode;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Encoder implements Action {
 
 
+    @Override
     public Result run(String[] parameters) {
         String inputFile = parameters[0];
         String outputFile = parameters[1];
         String encryptionKeyString = parameters[2];
-        int encryptionKey = Integer.parseInt( encryptionKeyString );
-        validateParrams( inputFile, outputFile, encryptionKeyString );
-        encoding( inputFile, outputFile, encryptionKey );
-        return new Result( "no errors detected,", ResultCode.OK );
+        toEncoder( inputFile, outputFile, encryptionKeyString );
+        return new Result( "File is Encode", ResultCode.OK );
     }
 
-    private void encoding(String inputFile, String outputFile, int encryptionKey) {
-        try (Scanner fileReader = new Scanner( Files.newBufferedReader( Path.of( inputFile ) ) );
-             BufferedWriter fileWriter = Files.newBufferedWriter( Path.of( outputFile ) )) {
+    private void toEncoder(String inputFile, String outputFile, String encryptionKeyString) {
+        int encryptionKeyValue;
 
-            //Gets modulus of a number
-            int encKey = Math.abs( encryptionKey % Constants.ALPHABET.size() );
-            String line;
-            while (fileReader.hasNextLine()) {
-                line = fileReader.nextLine();
-                char[] chars = line.toLowerCase().toCharArray();
-                char[] decodedChars = new char[chars.length];
-                int alphabetPos = -1;
-                int newPos;
+        encryptionKeyValue = Integer.parseInt( encryptionKeyString );
+        try (BufferedReader bufferedReader = new BufferedReader( new FileReader( inputFile ) );
+             BufferedWriter bufferedWriter = new BufferedWriter( new FileWriter( outputFile ) )) {
+            ArrayList<String> data = new ArrayList<>();
 
-                //Decoding new line.
+            while (bufferedReader.ready()) {
+                String string = bufferedReader.readLine();
+                char[] chars = string.toCharArray();
                 for (int i = 0; i < chars.length; i++) {
-                    char charsArray = chars[i];
-                    //finding symbol in ALPHABET.
-                    for (int j = 0; j < Constants.ALPHABET.size(); j++) {
-                        if (charsArray == Constants.ALPHABET.get( j )) {
-                            newPos = j - encKey;
-                            if (newPos < 0) {
-                                alphabetPos = newPos + Constants.ALPHABET.size();
-                            } else {
-                                alphabetPos = newPos;
-                            }
-                            break;
-                        }
+                    int index = Constants.ALPHABET.indexOf( Character.toLowerCase( chars[i] ) );
+                    if (index == -1) {
+                        continue;
                     }
-                    if (alphabetPos < 0) {
-                        decodedChars[i] = chars[i];
-                    } else {
-                        decodedChars[i] = Constants.ALPHABET.get( alphabetPos );
-                        alphabetPos = -1;
-                    }
+                    int shift = (index + encryptionKeyValue) % Constants.ALPHABET.size();
+                    if (shift < 0) shift = shift + Constants.ALPHABET.size();
+                    chars[i] = Constants.ALPHABET.get( shift );
                 }
-                fileWriter.write( decodedChars );
-
-                if (fileReader.hasNextLine()) {
-                    fileWriter.write( "\r\n" );
-                }
+                data.add( new String( chars ) );
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private void validateParrams(String inputFile, String outputFile, String encryptionKeyString) {
-        if (!Files.exists( Path.of( inputFile ) )) {
-            System.err.println( "ERROR. Input file must exist." );
-        }
-        if (!outputFile.endsWith( ".txt" )) {
-            System.err.println( "ERROR. Output file name must be *.txt." );
-        }
-        if (!Files.exists( Path.of( outputFile ) )) {
-            try {
-                Files.createFile( Path.of( outputFile ) );
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.err.println( "Couldn't create output file." );
+            for (String string : data) {
+                bufferedWriter.write( string + "\n" );
             }
-        }
-        try {
-            int encryptionKey = Integer.parseInt( encryptionKeyString );
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-
+        } catch (IOException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
         }
     }
 }
