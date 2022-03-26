be9bf05906f275df232606de2dd126f4eed9ede2
a.nikolaev <sparkton02@gmail.com>
2022-03-26 18:09:28 +0500

    If ConsoleRunner catch exception ex he is exited with status 1

--- src/main/java/ru/javarush/nikolaev/cryptoanalyzer/ConsoleRunner.java
+++ src/main/java/ru/javarush/nikolaev/cryptoanalyzer/ConsoleRunner.java
@@ -1,13 +1,18 @@
 package ru.javarush.nikolaev.cryptoanalyzer;
 
 import ru.javarush.nikolaev.cryptoanalyzer.entity.Result;
+import ru.javarush.nikolaev.cryptoanalyzer.exceptions.AppException;
 
 public class ConsoleRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AppException {
         Application application = new Application();
-        Result result = application.run( args );
-        System.out.println( result );
-        args.toString();
+        try {
+            Result result = application.run( args );
+        } catch (AppException exception) {
+            exception.getMessage();
+        } finally {
+            System.exit( 1 );
+        }
     }
 }
 
